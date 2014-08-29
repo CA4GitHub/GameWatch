15100bba0db36495592019a684779ec98a384753
CA4GitHub <flawton@fredricsmacbook.att.net>
2014-08-29 02:07:17 -0700

    Added a login view controller
    Added FLLoginViewController.
    Used 3rd party files from GitHub to fix keyboard hiding text issue (as
    done on teamtreehouse ribbit app).

--- GameWatch/FLLoginViewController.m
+++ GameWatch/FLLoginViewController.m
@@ -10,6 +10,10 @@
 
 @interface FLLoginViewController ()
 
+
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+
 @end
 
 @implementation FLLoginViewController
@@ -45,5 +49,29 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
+#pragma mark - IBActions
+- (IBAction)logIn:(id)sender {
+    NSLog(@"Log In");
+}
+- (IBAction)signUp:(id)sender {
+    NSLog(@"Sign Up");
+}
+- (IBAction)enableSecureText:(id)sender {
+    //hide password as type
+    self.password.secureTextEntry = YES;
+}
+
+- (IBAction)hideKeyboard:(id)sender {
+    //called when any of the following occurs
+    //1) user presses done button on keyboard for username or password text fields
+    //2) user has keyboard showing and taps on non-keyboard part of the screen
+    
+    //Sam's iOS6 p.216
+    [self.username resignFirstResponder];
+    [self.password resignFirstResponder];
+    
+}
 
 @end
