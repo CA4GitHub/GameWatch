a8f5eaf46605bef2d6a127c2240d26d9fe50e692
CA4GitHub <flawton@fredricsmacbook.att.net>
2014-08-28 19:31:56 -0700

    Initial Commit

---
+++ GameWatch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GameWatch
+//
+//  Created by Fredrick Lawton on 8/28/14.
+//  Copyright (c) 2014 Fredrick A. Lawton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GameWatch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GameWatch
+//
+//  Created by Fredrick Lawton on 8/28/14.
+//  Copyright (c) 2014 Fredrick A. Lawton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
